dadae3bc90f80245fa7c95ba61d3d4b3b37871cd
Alex McTammany <apmctammany@gmail.com>
2025-12-20 15:42:00 -0500

    fix leak env

--- auth.ts
+++ auth.ts
@@ -1,9 +1,4 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
-console.log(
-  "Auth Config:",
-  process.env.AUTH_GOOGLE_ID,
-  process.env.AUTH_GOOGLE_SECRET
-);
 
 export const { handlers, signIn, signOut, auth } = NextAuth(authConfig);
