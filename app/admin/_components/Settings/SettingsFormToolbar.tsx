ed7d667ed00be2469d15d0c8cd14b33f7828e2d9
Alex McTammany <apmctammany@gmail.com>
2025-12-22 20:16:06 -0500

    btngroup + router

--- app/admin/_components/Settings/SettingsFormToolbar.tsx
+++ app/admin/_components/Settings/SettingsFormToolbar.tsx
@@ -4,22 +4,32 @@ import React, { useContext } from "react";
 import { useFormContext } from "react-hook-form";
 import { UserType } from "./SettingsForm";
 import { Button } from "@/components/ui/button";
+import { ButtonGroup } from "@/components/ui/button-group";
+import { useRouter } from "next/navigation";
 
 export default function SettingsFormToolbar() {
   const formContext = useFormContext<UserType>();
   const { state, undo, redo, handleRedo, handleUndo, canRedo, canUndo } =
     useContext(RevisionContext)!;
+  const router = useRouter();
   console.log(state, canRedo, canUndo);
 
   return (
-    <div>
-      <Button onClick={handleUndo} disabled={!canUndo}>
-        Undo
-      </Button>
-      <Button onClick={handleRedo} disabled={!canRedo}>
-        Redo
-      </Button>
-      <Button type="submit">Save</Button>
-    </div>
+    <ButtonGroup>
+      <ButtonGroup>
+        <Button onClick={handleUndo} disabled={!canUndo}>
+          Undo
+        </Button>
+        <Button onClick={handleRedo} disabled={!canRedo}>
+          Redo
+        </Button>
+      </ButtonGroup>
+      <ButtonGroup>
+        <Button variant="destructive" onClick={() => router.back()}>
+          Cancel
+        </Button>
+        <Button type="submit">Save</Button>
+      </ButtonGroup>
+    </ButtonGroup>
   );
 }
