ac5cf959832ed444cd086704dfb78924c5f4bf25
Alex McTammany <apmctammany@gmail.com>
2025-12-28 08:34:14 -0500

    foo

--- lib/authClient.ts
+++ lib/authClient.ts
@@ -10,5 +10,5 @@ import { auth } from "@/auth";
 export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || "http://localhost:3000",
 
-  plugins: [customSessionClient<typeof auth>()],
+  plugins: [customSessionClient<typeof auth>(), usernameClient()],
 });
