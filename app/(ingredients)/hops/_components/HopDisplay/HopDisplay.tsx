371a838a3b346f458966c26010ef9e350add4bcd
Alex McTammany <apmctammany@gmail.com>
2026-01-08 01:14:37 -0500

    good

--- app/(ingredients)/hops/_components/HopDisplay/HopDisplay.tsx
+++ app/(ingredients)/hops/_components/HopDisplay/HopDisplay.tsx
@@ -11,6 +11,7 @@ import { AdjustedHopType } from "@/types/Ingredient";
 import React from "react";
 import { Tabs, TabsTrigger, TabsContent, TabsList } from "@/components/ui/tabs";
 import dynamic from "next/dynamic";
+import { Hop } from "lucide-react";
 const HopDetailsTab = dynamic(() => import("./HopDetailsTab"));
 const HopPropertiesTab = dynamic(() => import("./HopPropertiesTab"));
 export type HopDisplayProps = {
@@ -20,9 +21,10 @@ export function HopDisplay({ src }: HopDisplayProps) {
   return (
     <Tabs defaultValue="details">
       <Card className="m-2 lg:m-4 py-1 lg:py-4">
-        <CardHeader className="border-b-4 grid md:grid-cols-2 p-2">
-          <CardTitle className="m-auto text-sm lg:text-2xl">
-            {src.name}
+        <CardHeader className="border-b-4 grid md:grid-cols-2 p-2 lg:px-4">
+          <CardTitle className="w-full text-sm lg:text-2xl flex">
+            <Hop className="inline mr-2 m-auto" />
+            <h3 className="grow px-2 lg:px-4">{src.name}</h3>
           </CardTitle>
           <TabsList className="m-auto w-full ">
             <TabsTrigger value="details">Details</TabsTrigger>
