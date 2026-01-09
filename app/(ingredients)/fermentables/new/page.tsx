fe96802c288dac69c5f1571be4580d1b7251e9e3
Alex McTammany <apmctammany@gmail.com>
2026-01-08 21:24:22 -0500

    push

--- app/(ingredients)/fermentables/new/page.tsx
+++ app/(ingredients)/fermentables/new/page.tsx
@@ -5,16 +5,19 @@ import { createFermentable } from "@/app/(ingredients)/fermentables/actions";
 import { getPreferences } from "@/app/admin/queries";
 import { AdjustedFermentableType, FermentableType } from "@/types/Ingredient";
 import { verifySession } from "@/lib/verifySession";
+import { getCountries } from "../../queries";
 
 export default async function FermentableCreatorPage() {
   const session = await verifySession("/fermentables/new");
   if (!session?.user) unauthorized();
   const prefs = await getPreferences();
+  const countries = await getCountries();
   const src = {
     userId: session.user.id,
   } as AdjustedFermentableType;
   return (
     <FermentableEditor
+      countries={countries}
       src={src}
       preferences={prefs}
       action={createFermentable.bind(null, prefs)}
--- app/(ingredients)/hops/new/page.tsx
+++ app/(ingredients)/hops/new/page.tsx
@@ -5,16 +5,19 @@ import { createHop } from "@/app/(ingredients)/hops/actions";
 import { getPreferences } from "@/app/admin/queries";
 import { AdjustedHopType, HopType } from "@/types/Ingredient";
 import { verifySession } from "@/lib/verifySession";
+import { getCountries } from "../../queries";
 
 export default async function HopCreatorPage() {
   const session = await verifySession("/hops/new");
   if (!session?.user) unauthorized();
   const prefs = await getPreferences();
+  const countries = await getCountries();
   const src = {
     userId: session.user.id,
   } as AdjustedHopType;
   return (
     <HopEditor
+      countries={countries}
       src={src}
       preferences={prefs}
       action={createHop.bind(null, prefs)}
