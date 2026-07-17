5158cc4238ad7651daa67131e8797f1e9c83ac9a
Alex McTammany <apmctammany@gmail.com>
2026-07-17 06:05:08 -0400

    fix recipe new

--- app/recipes/new/page.tsx
+++ app/recipes/new/page.tsx
@@ -5,12 +5,18 @@ import React from "react";
 import RecipeEditor from "../_components/RecipeEditor/RecipeEditor";
 import { createRecipe } from "../actions";
 import { getStyleNames } from "@/app/styles/queries";
+import { BaseUser } from "@/types/User";
 
 export default async function RecipeCreatorPage() {
   const session = await verifySession("/recipes/new");
   if (!session?.user) unauthorized();
+  const owner: Partial<BaseUser> = {
+    id: session.user.id,
+    name: session.user.name || "",
+  };
   const src = {
     userId: session.user.id,
+    owner,
   } as RecipeType;
   const styles = getStyleNames();
   return (
